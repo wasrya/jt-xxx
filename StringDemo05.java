30ff9a04af03019dba083d29d119552fc9099817
runoob <test@runoob.com>
2018-03-09 10:51:15 +0800

    2018391049

---
+++ StringDemo05.java
@@ -0,0 +1,56 @@
+package day01;
+
+public class StringDemo05 {
+
+	public static void main(String[] args) {
+		/**
+		 * 查找字符串
+		 */
+		String s = "ABC123中国在中国的位置";
+		//在s中查找“中国”出现的位置
+		//匹配到第一个位置就返回这个位置
+		int n = s.indexOf("中国");
+		System.out.println(n);
+		//如果查找不到，返回-1
+		n = s.indexOf("美国");
+		System.out.println(n);//-1
+		
+		//一个重载的 indexOf方法
+		// s.indexOf("字符串", 起始位置)
+		// 从“起始位置”开始查找 “字符串”
+		
+		s = "画上荷花和尚画";
+		//   0 1 2 3 4 5 6
+		//查找第二个 “画”的位置
+		n = s.indexOf("画", 1);
+		System.out.println(n);//6
+		
+		//测试重载的方法：查找一个字符
+		n = s.indexOf('画', 1);
+		System.out.println(n);//6
+		
+		//自行测试：从头查找一个字符
+		// indexOf(char)
+		
+		//lastIndexOf
+		n = s.lastIndexOf('画');
+		System.out.println(n); 
+		n = s.lastIndexOf('和');
+		System.out.println(n); 
+		//经典案例：查找最后一个“点”的位置
+		String url=
+			"http://www.tedu.cn/index.html";
+		n = url.lastIndexOf(".");
+		System.out.println(n); 
+	
+	}
+
+}
+
+
+
+
+
+
+
+
---
+++ StringDemo07.java
@@ -0,0 +1,37 @@
+package day01;
+
+public class StringDemo07 {
+
+	public static void main(String[] args) {
+		/**
+		 * 去除字符串两端的“空白”
+		 * 空白：空格，回车\n，tab\t等
+		 */
+		String name = " \t Tom \n \r  ";
+		String n = name.trim();
+		System.out.println("["+n+"]");
+		System.out.println("["+name+"]");
+		
+		String s = "  Tom ";
+		s.trim();
+		System.out.println(s);
+		//如上代码输出结果是：
+		//A.Tom  B.null  C.空串  D.  Tom 
+		
+		//检查 trim() 返回的是否是原字符串
+		String s1 = "ABC";
+		String s2 = s1.trim();
+		System.out.println(s1==s2); 
+		String s3 = " ABC ";
+		String s4 = s3.trim();
+		System.out.println(s3==s4); 
+	}
+}
+
+
+
+
+
+
+
+
